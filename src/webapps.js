3ccaced93249b323c64243171a301e820789cd15
kwafelt <112316378+kwafelt@users.noreply.github.com>
2022-09-29 00:21:03 +0800

    Update webapps.js

--- src/webapps.js
+++ src/webapps.js
@@ -7,7 +7,7 @@ let webapps = function(num){
   let mn = $EL('tag','main')[0]; mn.textContent = null;
   break
   default:
-   mp3 = '= './src/audio/petualang.mp3?cache=true';
+   mp3 = './src/audio/petualang.mp3?cache=true';
    let m4a = new Audio(); m4a.src = mp3 ; m4a.preload = 'metadata'; m4a.oncanplay = m4a.play();
    $EL('tag','main')[0].textContent = 'Anda telah terjebak dalam honeypot, cuba check link url baik-baik ~';
  };
