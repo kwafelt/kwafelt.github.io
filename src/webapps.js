06b9a8f2b77b4b19372a61fb91b4c731f238c3ca
kwafelt <112316378+kwafelt@users.noreply.github.com>
2022-11-13 00:19:14 +0800

    Update webapps.js

--- src/webapps.js
+++ src/webapps.js
@@ -1,14 +1,1 @@
 
-let webapps = function(num){
- let mn = $EL('tag','main')[0]; mn.textContent = null;
- switch (num){
-  case 0:
-  tx = 'ddsdsd'
-  let mn = $EL('tag','main')[0]; mn.textContent = null;
-  break
-  default:
-   mp3 = './src/audio/Toxic.m4a?cache=true';
-   let m4a = new Audio(); m4a.src = mp3 ; m4a.preload = 'metadata'; m4a.oncanplay = m4a.play();
-   $EL('tag','main')[0].textContent = 'Anda telah terjebak dalam honeypot, cuba check link url baik-baik ~';
- };
-};
