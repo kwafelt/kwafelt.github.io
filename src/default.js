d5c189b86bd291ddb94125636d2418cb492122eb
kwafelt <112316378+kwafelt@users.noreply.github.com>
2022-12-14 21:57:42 +0800

    kemaskini-2022-12-14

--- src/default.js
+++ src/default.js
@@ -12,8 +12,6 @@ let startr = function(){console.log(`stack:${hostnm}${portnm}${pathnm}?payload=s
 //
 let library = [`625:781`,`738:740`,`740:741`,`631:846`,`640:771`,`725:760`,`668:482`,`627:629`,`690:699`,`746:801`,`750:885`,`545:658`,`686:826`,`678:874`,`726:855`]
 
-let time = new Date(); let years = time.getFullYear();
-
 if (window.addEventListener){window.addEventListener(`DOMContentLoaded`,startr,false)}// W3C standard
 else if (window.attachEvent){window.attachEvent(`onload`,startr)}// Microsoft
 //:
@@ -97,19 +95,18 @@ let guestw = function(init){
 let questr = function(init){
   let que = self.location.search; let sub = que.substr(1,que.length);
   let inc = que.includes(`?`); if (inc == true){
-  active(sub);} else { void(0);}
+  active(`none`);} else { void(0);}
 }
 //
 let active = function(init){
   console.log(`re-write: ${document.readyState}`); switch (init){
-  case `debug`: errors(1020);
+  case `none`: window.location.replace(`${origin}`);
+  break; case `debug`: errors(1020);
   break; case `stt=ott`: deface(`pass`);
   break; case `stt=err`: er4 = `
   <p class="er"><span class="e1">404 - PAGE NOT FOUND</span><br>The link is broken or the page<br>has been moved.</p>`
   const err = $EL(`cre`,`section`); err.className = `errors cgrid`;
   $EL(`tag`,`main`)[0].replaceChildren(err); err.innerHTML = er4;
-  break; case `stt=dir`: alert(`dirbuster`);
-  break; case `stt=cli`: alert(`script`)
   break; case `ctf=dfm`: EVT = `
   <p class="p0">.EVENT-CLOSED</p>`;
   const CTF = $EL(`cre`,`section`); CTF.className = `errors panel_grid`;
@@ -122,7 +119,8 @@ let active = function(init){
 }
 //
 let custom = function(init){
-  cdat0 = init.substr(years); cdat1 = bssix4(`dec`,cdat0);
+  let bLOb = Number(window.status); let v4L0 = ((bLOb*2) - 20);
+  let cdat0 = init.substr(v4L0); let cdat1 = bssix4(`dec`,cdat0);
   const acc = $EL(`cre`,`section`); acc.className = `secret flex`; acc.innerHTML = cdat1;
   $EL(`tag`,`main`)[0].replaceChildren(acc); $EL(`tag`,`body`)[0].className = `dark`;
 }
@@ -137,7 +135,6 @@ let deface = function(init){
 };
 //
 let errors = function(init,raw){
-
   switch(init){
   case `404`:
   break; case `500`:
@@ -148,7 +145,6 @@ let errors = function(init,raw){
   <p class="s01">Error 1020</p><p class="s02">Access denied</p><p class="s03">What happened?</p>
   <p class="s04">This website is using a security service to protect itself from online attacks.</p>
   <p class="cli"><a href="javascript:biskut(0);">continue</a></p>
-  
   <p class="s05">Cloudflare Ray ID: 623d572b1e6123e0 • Your IP: ${raw} • Performance & security by Cloudflare</p>`
   const acc = $EL(`cre`,`section`); acc.className = `errors flex`; acc.innerHTML = ac;
   $EL(`tag`,`main`)[0].replaceChildren(acc); $EL(`tag`,`main`)[0].style = ``;
@@ -156,88 +152,13 @@ let errors = function(init,raw){
   }
 }
 //
-let activeorig = function(init){
-  let que = window.location.search.substr(1,wr.length);
-  num = (que == `stt=ott` ? 0 : que == `stt=err` ? 1 : ws == `ctf=dfm` ? 2 : ws == `ctf=log` ? 3 : 9);
-  que = wr.includes(`?`); if (que){wbapps(num);} else {deface(1);}
-};// honeyp ? honeypot : index; 
-//
-let verify = function(c1,c2){
-  return (c1 * c2).toString().substr(2,4);
-};
-let wbapps = function(num){
-  console.log(`re-write: ${document.readyState}`); switch (num){
-    case 0: tools = `<p class="p0">..EVENT-CLOSED</p>`;
-    const ott = $EL(`cre`,`section`); ott.className = `tools`; ott.innerHTML = tools;
-//  let m4a = new Audio(); m4a.src = `./audio/Botingkek.m4a?cache=true`;
-//  m4a.preload = `metadata`; m4a.oncanplay = m4a.play();
-    $EL(`tag`,`main`)[0].replaceChildren(ott);
-    break; case 1:
-    $EL(`tag`,`main`)[0].replaceChildren(``); deface(1);
-    break; case 2:
-    accs = `
-    <p class="p1">Enter your authorized 'CTF' access code</p><form action="javascript:txtacc(0);" name="access">
-    <input type="text" id="code" name="code" maxlength="6"><input type="hidden" value="submit"></form>
-    <p class="p1">hint: kod dari thread forum</p>`
-    const s2 = $EL(`cre`,`section`); s2.className = `access panel_grid`; s2.innerHTML = accs;
-    $EL(`tag`,`main`)[0].replaceChildren(s2); $EL(`tag`,`input`)[0].focus();
-    break; case 3:
-    pass = `
-    <p class="p0">STAFF-ONLY</p>
-    <form action="javascript:staff(1);" method="post" name="staffl">
-    <input type="text" placeholder="Enter Username" maxlength="7" name="uname" id="usern" required>
-    <input type="password" placeholder="Enter Password" maxlength="8" name="passw" id="passw" required>
-    <input type="submit" value="Login"></form>
-    <p class="p1"><br><a onclick="staff(0)">Forgot password ?</a></p>`
-    const s3 = $EL(`cre`,`section`); s3.className = `access panel_grid`; s3.innerHTML = pass;
-    $EL(`tag`,`main`)[0].replaceChildren(s3); $EL(`tag`,`input`)[0].focus();
-    break; default:
-    err4 = `
-    <p class="er"><span class="e1">This site can’t be reach</span><br><span class="e2">${hostnm}${pathnm} refused to connect.</span>
-    <br><code class="mono"><br>ERR_CONNECTION_REFUSED</code></p>`
-    const xx = $EL(`cre`,`section`); xx.className = `errors panel_grid`;
-    $EL(`tag`,`main`)[0].replaceChildren(xx); xx.innerHTML = err4;
-  };
+let verify = function(init){
+  const t1 = init.split(``); let r1 = 0; for (let i1 in t1){
+    r2 = t1[i1].codePointAt(0); r3 = Number(r2); r1 = r1 + r3;
+  }; return r1
 };
 //
-let menu = function(init){// masukkan onclick dalam menu
-  var m = $EL('que','.start a'); m.length; for (var i = 0; i < m.length; i++){
-    m[i].addEventListener('click',function(){goTo(this.dataset.r)},false);
-};};
-//
-let goTo = function(init){permissions('granted')}
-
 let datatxt = function(DAT){
   const DOC = window.open(``,``,``,``);
   DOC.document.write(DAT); DOC.focus(); DOC.onblur = function(){DOC.close();}
 }
-let process = function(init){
-  let ssL = init.substr(c4,init.length); ssM = base64('dec',ssL);
-  endr = `<p class="pE"><code style="">${ssM}</code></p>`
-  const s2 = $EL(`cre`,`section`); s2.className = `endr`; s2.innerHTML = endr;
-  $EL(`tag`,`main`)[0].replaceChildren(s2); //self.location.replace(`${origin}`);
-}; let RESETR = function(init){readDOC(init, process)}
-
-let projection = function(){
- var fllscrn = $EL('tag','body')[0];
- if ((document.fullscreenElement == null || document.fullscreenElement == undefined) && (document.webkitFullscreenElement == null || document.webkitFullscreenElement == undefined)){// go full-screen
-  if (fllscrn.requestFullscreen){fllscrn.requestFullscreen()}
-  else if (fllscrn.webkitRequestFullscreen){fllscrn.webkitRequestFullscreen()}
- } else {// exit full-screen
-  if (document.exitFullscreen){document.exitFullscreen()}
-  else if (document.webkitExitFullscreen){document.webkitExitFullscreen()}
-  };
-};// kemaskini 20 nov 2022 //let text = String.fromCharCode(72, 69, 76, 76, 79);
-
-
-draft2 = ` let reverseString = function(str){return str.split("").reverse().join("");}; txt = reverseString(3432);`
-draft = `
-let section = function(init){
- let element = $EL('que',init);
- if(typeof(element) != 'undefined' && element != null){
-  let elemnt = $EL('cre',init); alert('OK')
- } else {
-  let create = $EL('cre',init); alert('NEW');
- }
-}
-`
